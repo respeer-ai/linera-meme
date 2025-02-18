01ff66e538f83ee74076f5147565701ab628b55f
Zhao KK <kikakkz@hotmail.com>
2025-02-18 10:34:31 +0000

    Add multi chain test

--- proxy/tests/multi_chain.rs
+++ proxy/tests/multi_chain.rs
@@ -6,7 +6,7 @@
 #![cfg(not(target_arch = "wasm32"))]
 
 use linera_sdk::{
-    base::{BytecodeId, Owner},
+    base::BytecodeId,
     test::{QueryOutcome, TestValidator},
 };
 use serde_json::json;
@@ -17,15 +17,14 @@ use std::str::FromStr;
 /// Creates the application on a `chain`, initializing it with a 42 then adds 15 and obtains 57.
 /// which is then checked.
 #[tokio::test(flavor = "multi_thread")]
-async fn single_chain_test() {
+async fn multi_chain_test() {
     let (validator, bytecode_id) =
         TestValidator::with_current_bytecode::<proxy::ProxyAbi, (), proxy::InstantiationArgument>()
             .await;
+
     let mut chain = validator.new_chain().await;
 
     let meme_bytecode_id = BytecodeId::from_str("58cc6e264a19cddf027010db262ca56a18e7b63e2a7ad1561ea9841f9aef308fc5ae59261c0137891a342001d3d4446a26c3666ed81aadf7e5eec6a01c86db6d").unwrap();
-    let owner = Owner::from_str("02e900512d2fca22897f80a2f6932ff454f2752ef7afad18729dd25e5b5b6e00")
-        .unwrap();
 
     let application_id = chain
         .create_application(
@@ -40,22 +39,5 @@ async fn single_chain_test() {
         .graphql_query(application_id, "query { memeBytecodeId }")
         .await;
     let expected = json!({"memeBytecodeId": meme_bytecode_id});
-    assert_eq!(response, expected);
-
-    chain
-        .add_block(|block| {
-            block.with_operation(
-                application_id,
-                proxy::ProxyOperation::ProposeAddGenesisMiner { owner },
-            );
-        })
-        .await;
-
-    let QueryOutcome { response, .. } = chain
-        .graphql_query(application_id, "query { genesisMiners }")
-        .await;
-
-    let expected = json!({"genesisMiners": [owner]});
-
     assert_eq!(response, expected);
 }
