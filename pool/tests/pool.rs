c265f3df4f1e3d0a1d94762a07d1ec26a1b6c4ce
Zhao KK <kikakkz@hotmail.com>
2025-03-12 07:46:50 +0000

    Support integration tests of pool

--- pool/tests/pool.rs
+++ pool/tests/pool.rs
@@ -46,6 +46,7 @@ struct TestSuite {
 
     initial_supply: Amount,
     initial_liquidity: Amount,
+    initial_native: Amount,
 }
 
 impl TestSuite {
@@ -78,6 +79,7 @@ impl TestSuite {
 
             initial_supply: Amount::from_tokens(21000000),
             initial_liquidity: Amount::from_tokens(11000000),
+            initial_native: Amount::from_tokens(10),
         }
     }
 
@@ -92,6 +94,13 @@ impl TestSuite {
         Account {
             chain_id: chain.id(),
             owner: Some(AccountOwner::User(Owner::from(chain.public_key()))),
+        }
+    }
+
+    fn application_account(&self, chain_id: ChainId, application_id: ApplicationId) -> Account {
+        Account {
+            chain_id,
+            owner: Some(AccountOwner::Application(application_id.forget_abi())),
         }
     }
 
@@ -135,7 +144,7 @@ impl TestSuite {
         )
     }
 
-    async fn create_meme_application(&mut self) {
+    async fn create_meme_application(&mut self, virtual_initial_liquidity: bool) {
         let instantiation_argument = MemeInstantiationArgument {
             meme: Meme {
                 name: "Test Token".to_string(),
@@ -163,9 +172,9 @@ impl TestSuite {
             creator: self.chain_owner_account(&self.meme_chain),
             initial_liquidity: Some(Liquidity {
                 fungible_amount: self.initial_liquidity,
-                native_amount: Amount::from_tokens(10),
+                native_amount: self.initial_native,
             }),
-            virtual_initial_liquidity: true,
+            virtual_initial_liquidity,
             swap_creator_chain_id: self.swap_chain.id(),
         };
 
@@ -188,7 +197,7 @@ impl TestSuite {
 /// Creates the application on a `chain`, initializing it with a 42 then adds 15 and obtains 57.
 /// which is then checked.
 #[tokio::test(flavor = "multi_thread")]
-async fn pool_test() {
+async fn pool_virtual_initial_liquidity_test() {
     let _ = env_logger::builder().is_test(true).try_init();
 
     let mut suite = TestSuite::new().await;
@@ -201,7 +210,7 @@ async fn pool_test() {
     suite.fund_chain(&meme_chain, OPEN_CHAIN_FEE_BUDGET).await;
 
     suite.create_swap_application().await;
-    suite.create_meme_application().await;
+    suite.create_meme_application(true).await;
 
     // Check initial swap pool
     meme_chain.handle_received_messages().await;
@@ -262,5 +271,134 @@ async fn pool_test() {
         .graphql_query(suite.pool_application_id.unwrap(), "query { pool }")
         .await;
     let pool: Pool = serde_json::from_value(response["pool"].clone()).unwrap();
-    log::info!("Pool {:?}", pool);
+
+    assert_eq!(OPEN_CHAIN_FEE_BUDGET, pool_chain.chain_balance().await);
+    assert_eq!(suite.initial_native, pool.reserve_1);
+    assert_eq!(suite.initial_liquidity, pool.reserve_0);
+
+    let pool_application_account = suite.application_account(
+        pool_chain.id(),
+        suite.pool_application_id.unwrap().forget_abi(),
+    );
+    let query = format!(
+        "query {{ balanceOf(owner: \"{}\")}}",
+        pool_application_account,
+    );
+    let QueryOutcome { response, .. } = meme_chain
+        .graphql_query(suite.meme_application_id.unwrap(), query)
+        .await;
+    assert_eq!(
+        Amount::from_str(response["balanceOf"].as_str().unwrap()).unwrap(),
+        suite.initial_liquidity,
+    );
 }
+
+/*
+#[tokio::test(flavor = "multi_thread")]
+async fn pool_real_initial_liquidity_test() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    let mut suite = TestSuite::new().await;
+    let meme_chain = &suite.meme_chain.clone();
+    let user_chain = &suite.user_chain.clone();
+    let swap_chain = &suite.swap_chain.clone();
+
+    let swap_key_pair = swap_chain.key_pair();
+
+    suite
+        .fund_chain(
+            &meme_chain,
+            OPEN_CHAIN_FEE_BUDGET.try_add(suite.initial_native).unwrap(),
+        )
+        .await;
+
+    suite.create_swap_application().await;
+    suite.create_meme_application(false).await;
+
+    // Check initial swap pool
+    meme_chain.handle_received_messages().await;
+    swap_chain.handle_received_messages().await;
+
+    let QueryOutcome { response, .. } = swap_chain
+        .graphql_query(
+            suite.swap_application_id.unwrap(),
+            "query { poolChainCreationMessages }",
+        )
+        .await;
+    assert_eq!(
+        response["poolChainCreationMessages"]
+            .as_array()
+            .unwrap()
+            .len(),
+        1,
+    );
+
+    let message_id = MessageId::from_str(
+        response["poolChainCreationMessages"].as_array().unwrap()[0]
+            .as_str()
+            .unwrap(),
+    )
+    .unwrap();
+
+    let description = ChainDescription::Child(message_id);
+    let pool_chain = ActiveChain::new(swap_key_pair.copy(), description, suite.clone().validator);
+
+    suite.validator.add_chain(pool_chain.clone());
+    suite.pool_chain = Some(pool_chain.clone());
+
+    pool_chain.handle_received_messages().await;
+    swap_chain.handle_received_messages().await;
+    meme_chain.handle_received_messages().await;
+
+    let QueryOutcome { response, .. } = swap_chain
+        .graphql_query(
+            suite.swap_application_id.unwrap(),
+            "query { pools {
+                poolId
+                token0
+                token1
+                poolApplication
+            } }",
+        )
+        .await;
+    assert_eq!(response["pools"].as_array().unwrap().len(), 1,);
+    let pool: PoolIndex =
+        serde_json::from_value(response["pools"].as_array().unwrap()[0].clone()).unwrap();
+
+    let Some(AccountOwner::Application(pool_application_id)) = pool.pool_application.owner else {
+        panic!("Invalid pool application");
+    };
+    suite.pool_application_id = Some(pool_application_id.with_abi::<PoolAbi>());
+
+    let QueryOutcome { response, .. } = pool_chain
+        .graphql_query(suite.pool_application_id.unwrap(), "query { pool }")
+        .await;
+    let pool: Pool = serde_json::from_value(response["pool"].clone()).unwrap();
+
+    assert_eq!(
+        suite.initial_native.try_add(OPEN_CHAIN_FEE_BUDGET).unwrap(),
+        pool_chain
+            .owner_balance(&AccountOwner::Application(pool_application_id))
+            .await
+            .unwrap()
+    );
+    assert_eq!(suite.initial_native, pool.reserve_1);
+    assert_eq!(suite.initial_liquidity, pool.reserve_0);
+
+    let pool_application_account = suite.application_account(
+        pool_chain.id(),
+        suite.pool_application_id.unwrap().forget_abi(),
+    );
+    let query = format!(
+        "query {{ balanceOf(owner: \"{}\")}}",
+        pool_application_account,
+    );
+    let QueryOutcome { response, .. } = meme_chain
+        .graphql_query(suite.pool_application_id.unwrap(), query)
+        .await;
+    assert_eq!(
+        Amount::from_str(response["balanceOf"].as_str().unwrap()).unwrap(),
+        suite.initial_liquidity,
+    );
+}
+*/
