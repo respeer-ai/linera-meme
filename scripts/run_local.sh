c5f5b6132d9dc7cb1c086af146f79eb089a5aa89
Zhao KK <kikakkz@hotmail.com>
2025-03-21 13:49:34 +0000

    Update nginx template

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -326,16 +326,21 @@ function generate_nginx_conf() {
     echo "cp ${CONFIG_DIR}/$endpoint.nginx.conf /etc/nginx/sites-enabled/"
 }
 
-echo "http://api.blobgateway.com/api/blobs/chains/$BLOB_GATEWAY_CHAIN_ID/applications/$BLOB_GATEWAY_APPLICATION_ID"
-echo "http://api.ams.respeer.ai/api/ams/chains/$AMS_CHAIN_ID/applications/$AMS_APPLICATION_ID"
-echo "http://api.linerameme.fun/api/proxy/chains/$PROXY_CHAIN_ID/applications/$PROXY_APPLICATION_ID"
-echo "http://api.lineraswap.fun/api/swap/chains/$SWAP_CHAIN_ID/applications/$SWAP_APPLICATION_ID"
-
 # Generate service nginx conf
 generate_nginx_conf 20080 blobs api.blobgateway.com
 generate_nginx_conf 21080 ams api.ams.respeer.ai
 generate_nginx_conf 22080 swap api.lineraswap.fun
 generate_nginx_conf 23080 proxy api.linerameme.fun
+
+echo -e "\n\nService domain"
+echo -e "   $LAN_IP api.blobgateway.com"
+echo -e "   $LAN_IP api.ams.respeer.ai"
+echo -e "   $LAN_IP api.linerameme.fun"
+echo -e "   $LAN_IP api.lineraswap.fun"
+echo -e "   http://api.blobgateway.com/api/blobs/chains/$BLOB_GATEWAY_CHAIN_ID/applications/$BLOB_GATEWAY_APPLICATION_ID"
+echo -e "   http://api.ams.respeer.ai/api/ams/chains/$AMS_CHAIN_ID/applications/$AMS_APPLICATION_ID"
+echo -e "   http://api.linerameme.fun/api/proxy/chains/$PROXY_CHAIN_ID/applications/$PROXY_APPLICATION_ID"
+echo -e "   http://api.lineraswap.fun/api/swap/chains/$SWAP_CHAIN_ID/applications/$SWAP_APPLICATION_ID\n\n"
 
 function run_service() {
     wallet_name=$1
