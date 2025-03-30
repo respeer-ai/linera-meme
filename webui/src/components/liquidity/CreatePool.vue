d335931f184083d7134643e5e1a292c7939ece36
Zhao KK <kikakkz@hotmail.com>
2025-03-30 11:48:12 +0800

    Move balance of meme to store

--- webui/src/components/liquidity/CreatePool.vue
+++ webui/src/components/liquidity/CreatePool.vue
@@ -1,76 +1,78 @@
 <template>
-  <div class='bg-white popup-padding'>
-    <h5>Create pool</h5>
-    <q-separator />
-    <q-card flat class='bg-red-1 border-radius-8px popup-padding vertical-inner-y-margin'>
-      <div class='row'>
-        <div>
-          <div class='text-bold'>
-            {{ token0Ticker }}
-          </div>
-          <div class='text-grey-8'>
-            {{ shortId(token0 || '', 5) }}
-          </div>
-        </div>
-        <q-space />
+  <q-page class='flex items-center justify-center'>
+    <q-card flat class='create-pool-card'>
+      <h5 class='text-center text-bold text-grey-8'>Create pool</h5>
+      <q-separator />
+      <q-card flat class='bg-red-1 border-radius-8px popup-padding vertical-inner-y-margin'>
         <div class='row'>
-          <q-icon name='bi-wallet-fill text-grey-8 swap-amount-icon' size='16px' />
-          <div class='swap-amount-label text-grey-9 text-bold'>
-            {{ Number(outBalance).toFixed(2) }}
+          <div>
+            <div class='text-bold'>
+              {{ token0Ticker }}
+            </div>
+            <div class='text-grey-8'>
+              {{ shortId(token0 || '', 5) }}
+            </div>
           </div>
-          <div class='text-grey-8'>
-            {{ token0Ticker }}
-          </div>
-        </div>
-      </div>
-      <div class='row vertical-card-align swap-token'>
-        <q-input
-          class='swap-amount-input text-grey-8' dense v-model.number='token0Amount' reverse-fill-mask
-          input-class='text-right'
-          :error='token0AmountError'
-        />
-      </div>
-    </q-card>
-    <div class='row vertical-card-align'>
-      <div class='decorate-border-bottom-bold exchange-separator' />
-      <div class='exchange-symbol' size='28px'>
-        <q-icon name='bi-three-dots' size='14px' class='text-grey-6' />
-      </div>
-      <div class='decorate-border-bottom-bold exchange-separator' />
-    </div>
-    <q-card flat class='bg-red-1 border-radius-8px popup-padding vertical-card-align'>
-      <div class='row'>
-        <div>
-          <div class='text-bold'>
-            {{ token1Ticker }}
-          </div>
-          <div class='text-grey-8'>
-            {{ shortId(token1 || '', 5) }}
+          <q-space />
+          <div class='row'>
+            <q-icon name='bi-wallet-fill text-grey-8 swap-amount-icon' size='16px' />
+            <div class='swap-amount-label text-grey-9 text-bold'>
+              {{ Number(outBalance).toFixed(2) }}
+            </div>
+            <div class='text-grey-8'>
+              {{ token0Ticker }}
+            </div>
           </div>
         </div>
-        <q-space />
+        <div class='row vertical-card-align swap-token'>
+          <q-input
+            class='swap-amount-input text-grey-8' dense v-model.number='token0Amount' reverse-fill-mask
+            input-class='text-right'
+            :error='token0AmountError'
+          />
+        </div>
+      </q-card>
+      <div class='row vertical-card-align'>
+        <div class='decorate-border-bottom-bold exchange-separator' />
+        <div class='exchange-symbol' size='28px'>
+          <q-icon name='bi-three-dots' size='14px' class='text-grey-6' />
+        </div>
+        <div class='decorate-border-bottom-bold exchange-separator' />
+      </div>
+      <q-card flat class='bg-red-1 border-radius-8px popup-padding vertical-card-align'>
         <div class='row'>
-          <q-icon name='bi-wallet-fill text-grey-8 swap-amount-icon' size='16px' />
-          <div class='swap-amount-label text-grey-9 text-bold'>
-            {{ Number(inBalance).toFixed(2) }}
+          <div>
+            <div class='text-bold'>
+              {{ token1Ticker }}
+            </div>
+            <div class='text-grey-8'>
+              {{ shortId(token1 || '', 5) }}
+            </div>
           </div>
-          <div class='text-grey-8'>
-            {{ token1Ticker }}
+          <q-space />
+          <div class='row'>
+            <q-icon name='bi-wallet-fill text-grey-8 swap-amount-icon' size='16px' />
+            <div class='swap-amount-label text-grey-9 text-bold'>
+              {{ Number(inBalance).toFixed(2) }}
+            </div>
+            <div class='text-grey-8'>
+              {{ token1Ticker }}
+            </div>
           </div>
         </div>
-      </div>
-      <div class='row vertical-card-align swap-token'>
-        <q-input
-          class='swap-amount-input' dense v-model.number='token1Amount' reverse-fill-mask
-          input-class='text-right'
-          :error='token1AmountError'
-        />
-      </div>
+        <div class='row vertical-card-align swap-token'>
+          <q-input
+            class='swap-amount-input' dense v-model.number='token1Amount' reverse-fill-mask
+            input-class='text-right'
+            :error='token1AmountError'
+          />
+        </div>
+      </q-card>
+      <q-btn
+        rounded flat :label='$t("MSG_CREATE_POOL")' class='full-width border-red-4 vertical-section-y-margin vertical-inner-y-margin-bottom'
+      />
     </q-card>
-    <q-btn
-      rounded flat :label='$t("MSG_ADD_LIQUIDITY")' class='full-width border-red-4 vertical-inner-y-margin vertical-inner-y-margin-bottom'
-    />
-  </div>
+  </q-page>
 </template>
 
 <script setup lang='ts'>
@@ -125,6 +127,12 @@ onMounted(() => {
 </script>
 
 <style scoped lang='sass'>
+.create-pool-card
+  border: 1px solid $red-4
+  border-radius: 16px
+  padding: 0 16px 16px 16px
+  width: 440px
+
 .swap-amount-label
   font-size: 20px
   margin-right: 4px
---
+++ webui/src/localstore/meme/wrapper.ts
@@ -0,0 +1,28 @@
+import { _Account, Account } from '../account'
+import { NotifyType } from '../notify'
+import { useUserStore } from '../user'
+import { useMemeStore } from './store'
+
+const _user = useUserStore()
+const _meme = useMemeStore()
+
+const balanceOfMeme = async (tokenApplication: Account, done: (balance: string) => void) => {
+  const owner = await _user.account()
+  if (!owner.owner || !tokenApplication.owner) return
+  const owenrDescription = _Account.accountDescription(owner)
+
+  _meme.balanceOf({
+    owner: owenrDescription,
+    Message: {
+      Error: {
+        Title: 'Balance of meme',
+        Message: 'Failed get balance of meme',
+        Popup: true,
+        Type: NotifyType.Error
+      }
+    }
+  }, tokenApplication, (error: boolean, balance?: string) => {
+    if (error) return
+    done(balance as string)
+  })
+}
