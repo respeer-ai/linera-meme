d4853d5be68f4455165bcb56f54bf964b73bd1b1
Zhao KK <kikakkz@hotmail.com>
2025-04-14 22:05:32 +0800

    Support update kline

--- webui/src/bridge/db/kline.ts
+++ webui/src/bridge/db/kline.ts
@@ -1,6 +1,8 @@
+import { BulkError } from 'dexie'
 import { dbKline } from 'src/controller'
 import { Interval } from 'src/localstore/kline/const'
 import { Point } from 'src/localstore/kline/types'
+import { dbModel } from 'src/model'
 
 export class Kline {
   static bulkPut = async (
@@ -11,15 +13,37 @@ export class Kline {
   ) => {
     const _points = points.map((point) => {
       return { ...point, token0, token1, interval }
-    })
+    }) as dbModel.KlinePoint[]
     const traceFunc = console.trace
     console.trace = () => {
       // DO NOTHING
     }
     try {
       await dbKline.klinePoints.bulkPut(_points)
-    } catch {
-      // DO NOTHING
+    } catch (e) {
+      // TODO: update exists item
+      const err = e as BulkError
+      if (err.name !== 'BulkError') return
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      for (const [pos, error] of Object.entries(err.failuresByPos)) {
+        try {
+          const _point = _points[parseInt(pos.toString())]
+          const { timestamp } = _point
+          const point = await dbKline.klinePoints.get([
+            token0,
+            token1,
+            interval,
+            timestamp
+          ])
+          if (!point) continue
+          _point.id = point.id
+          if (JSON.stringify(_point) === JSON.stringify(point)) continue
+          await dbKline.klinePoints.update(_point, _point)
+        } catch (e) {
+          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+          console.log(`Failed update point: ${e}`)
+        }
+      }
     }
     console.trace = traceFunc
   }
