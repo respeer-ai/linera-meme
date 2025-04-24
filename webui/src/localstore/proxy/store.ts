f26245aa0326ca9e5bc70991f5ade5604ed22a33
Zhao KK <kikakkz@hotmail.com>
2025-04-24 10:16:19 +0800

    Long connect to proxy service

--- webui/src/localstore/proxy/store.ts
+++ webui/src/localstore/proxy/store.ts
@@ -7,15 +7,33 @@ import { MEME_APPLICATIONS } from 'src/graphql'
 import { Chain } from 'src/__generated__/graphql/proxy/graphql'
 import { graphqlResult } from 'src/utils'
 import { Account } from '../account'
+import { _WebSocket, Notification } from 'src/websocket'
+import { constants } from 'src/constant'
 
 const options = /* await */ getClientOptions()
 const apolloClient = new ApolloClient(options)
 
 export const useProxyStore = defineStore('proxy', {
   state: () => ({
-    chains: [] as Array<Chain>
+    chains: [] as Array<Chain>,
+    websocket: undefined as unknown as _WebSocket
   }),
   actions: {
+    initializeProxy() {
+      this.websocket = new _WebSocket(constants.PROXY_HOST)
+      this.websocket.withOnMessage((notification) =>
+        this.onMessage(notification)
+      )
+      this.websocket.withOnError((e) => this.onError(e))
+    },
+    onMessage(notification: Notification) {
+      if (notification.notification === 'NewBlock') {
+        this.getApplications({})
+      }
+    },
+    onError(e: Event) {
+      console.log(`Proxy error: ${JSON.stringify(e)}`)
+    },
     getApplications(
       req: GetChainsRequest,
       done?: (error: boolean, rows?: Chain[]) => void
