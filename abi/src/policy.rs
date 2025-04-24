6e25c103a5b82d6a10e041fe3e7c317fb7a222ea
Zhao KK <kikakkz@hotmail.com>
2025-04-24 14:31:21 +0000

    Deduct 10 tokens as gas

--- abi/src/policy.rs
+++ abi/src/policy.rs
@@ -1,5 +1,5 @@
 use linera_sdk::linera_base_types::Amount;
 
 pub fn open_chain_fee_budget() -> Amount {
-    Amount::from_attos(Amount::ONE.saturating_div(Amount::from_attos(10)))
+    Amount::ONE.saturating_mul(10)
 }
