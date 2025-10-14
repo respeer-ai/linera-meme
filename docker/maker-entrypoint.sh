9a1df87de5955f21027d05acf79edaa06e973fe3
Zhao KK <kikakkz@hotmail.com>
2025-10-14 22:36:13 +0800

    Output log directly

--- docker/maker-entrypoint.sh
+++ docker/maker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 src/maker.py --swap-application-id "$SWAP_APPLICATION_ID" --wallet-host "$WALLET_HOST" --wallet-owner "$WALLET_OWNER" --wallet-chain "$WALLET_CHAIN" --swap-host "$SWAP_HOST" --proxy-host "$PROXY_HOST"
+python3 -u src/maker.py --swap-application-id "$SWAP_APPLICATION_ID" --wallet-host "$WALLET_HOST" --wallet-owner "$WALLET_OWNER" --wallet-chain "$WALLET_CHAIN" --swap-host "$SWAP_HOST" --proxy-host "$PROXY_HOST"
