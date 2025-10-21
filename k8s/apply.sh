a28601e4d1826eb55b50a4a9fbebe796839c5772
Zhao KK <kikakkz@hotmail.com>
2025-10-21 08:51:01 +0000

    Add apply script

---
+++ k8s/apply.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+kubectl apply -f 00-shared-app-data-pvc.yaml
+
+SERVICES="blob-gateway ams swap proxy"
+
+for service in $SERVICES; do
+  kubectl delete -f $service/02-deployment.yaml
+  kubectl delete -f $service/03-ingress.yaml
+
+  kubectl apply -f $service/02-deployment.yaml
+  kubectl apply -f $service/03-ingress.yaml
+
+  while true; do
+    count=$(kubectl get pods -A | grep ${service}-service | grep Running | wc -l)
+    if [ $count -eq 5 ]; then
+      break
+    fi
+    sleep 10
+  done
+done
