b208e8b11e754caf57e00654834a5bdbab2b38cd
Zhao KK <kikakkz@hotmail.com>
2025-10-22 06:26:34 +0000

    Correct deployment

--- k8s/apply.sh
+++ k8s/apply.sh
@@ -7,6 +7,16 @@ SERVICES="blob-gateway ams swap proxy"
 for service in $SERVICES; do
   kubectl delete -f $service/02-deployment.yaml
   kubectl delete -f $service/03-ingress.yaml
+done
+
+for service in $SERVICES; do
+  while true; do
+    count=$(kubectl get pods -A | grep ${service}-service | wc -l)
+    if [ $count -eq 0 ]; then
+      break
+    fi
+    sleep 10
+  done
 
   kubectl apply -f $service/02-deployment.yaml
   kubectl apply -f $service/03-ingress.yaml
