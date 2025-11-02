e4251624806d726783c1474665df11979cfa704c
Zhao KK <kikakkz@hotmail.com>
2025-11-02 11:55:31 +0000

    Add topology

--- k8s/apply.sh
+++ k8s/apply.sh
@@ -14,11 +14,6 @@ if [ $count -eq 0 ]; then
   kubectl create secret generic mysql-secret --from-literal=MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" --from-literal=MYSQL_PASSWORD="$MYSQL_PASSWORD" --namespace kube-system
 fi
 
-for service in $SERVICES; do
-  kubectl delete -f $service/02-deployment.yaml
-  kubectl delete -f $service/03-ingress.yaml
-done
-
 wait_pods() {
   pod_name=$1
   replicas=$2
@@ -35,6 +30,29 @@ wait_pods() {
 }
 
 for service in $SERVICES; do
+  kubectl delete -f $service/02-deployment.yaml
+  kubectl delete -f $service/03-ingress.yaml
+done
+
+kubectl delete -f mysql/02-deployment.yaml
+
+wait_pods mysql-service 0 ""
+
+kubectl delete -f kline/02-deployment.yaml
+kubectl delete -f kline/03-ingress.yaml
+
+wait_pods kline-service 0 ""
+
+kubectl delete -f funder/02-deployment.yaml
+
+wait_pods funder-service 0 ""
+
+kubectl delete -f maker/02-deployment.yaml
+
+wait_pods maker-service 0 ""
+wait_pods maker-wallet-service 0 ""
+
+for service in $SERVICES; do
   wait_pods ${service}-service 0 ""
 
   kubectl apply -f $service/01-strip-prefix.yaml
@@ -44,20 +62,11 @@ for service in $SERVICES; do
   wait_pods ${service}-service 5 Running
 done
 
-kubectl delete -f mysql/02-deployment.yaml
-
-wait_pods mysql-service 0 ""
-
 kubectl apply -f mysql/00-user.yaml
 kubectl apply -f mysql/01-pvc.yaml
 kubectl apply -f mysql/02-deployment.yaml
 
 wait_pods mysql-service 1 Running
-
-kubectl delete -f kline/02-deployment.yaml
-kubectl delete -f kline/03-ingress.yaml
-
-wait_pods kline-service 0 ""
 
 kubectl apply -f kline/01-strip-prefix.yaml
 kubectl apply -f kline/02-deployment.yaml
@@ -65,18 +74,9 @@ kubectl apply -f kline/03-ingress.yaml
 
 wait_pods kline-service 1 Running
 
-kubectl delete -f funder/02-deployment.yaml
-
-wait_pods funder-service 0 ""
-
 kubectl apply -f funder/02-deployment.yaml
 
 wait_pods funder-service 1 Running
-
-kubectl delete -f maker/02-deployment.yaml
-
-wait_pods maker-service 0 ""
-wait_pods maker-wallet-service 0 ""
 
 kubectl apply -f maker/01-pvc.yaml
 kubectl apply -f maker/02-deployment.yaml
