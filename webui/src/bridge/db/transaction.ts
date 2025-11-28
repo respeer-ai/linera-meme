a5ea7cb8c5fc267aef4f939a88a810038491dccc
Zhao KK <kikakkz@hotmail.com>
2025-11-28 08:53:11 +0800

    Support partially loading

--- webui/src/bridge/db/transaction.ts
+++ webui/src/bridge/db/transaction.ts
@@ -22,6 +22,24 @@ export class Transaction {
     console.trace = traceFunc
   }
 
+  static transactionsCount = async (
+    token0: string,
+    token1: string,
+    tokenReversed: number
+  ) => {
+    const from = [0, token0, token1, tokenReversed]
+    const to = [Number.MAX_SAFE_INTEGER, token0, token1, tokenReversed]
+
+    try {
+      return await dbKline.transactions
+        .where('[created_timestamp+token0+token1+token_reversed]')
+        .between(from, to)
+        .count()
+    } catch (e) {
+      console.log('Failed query', e)
+    }
+  }
+
   static transactions = async (
     token0: string,
     token1: string,
@@ -30,15 +48,21 @@ export class Transaction {
     timestampEnd?: number,
     limit?: number
   ) => {
-    const from = [timestampBegin ?? 0, token0, token1, tokenReversed]
-    const to = [timestampEnd ?? Number.MAX_SAFE_INTEGER, token0, token1, tokenReversed]
+    const startAt = timestampBegin ? timestampBegin * 1000 : 0
+    const endAt = timestampEnd ? timestampEnd * 1000 : Number.MAX_SAFE_INTEGER
+
+    const _startAt = startAt > endAt ? endAt : startAt
+    const _endAt = startAt > endAt ? startAt : endAt
+
+    const from = [_startAt, token0, token1, tokenReversed]
+    const to = [_endAt, token0, token1, tokenReversed]
 
     try {
       return await dbKline.transactions
         .where('[created_timestamp+token0+token1+token_reversed]')
         .between(from, to)
         .reverse()
-        .limit(limit ?? 999999)
+        .limit(limit ? limit * 2 : 999999)
         .toArray()
     } catch (e) {
       console.log('Failed query', e)
