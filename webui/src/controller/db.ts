fb19280921b5f781b5d16926b02c88b5af99abb0
Zhao KK <kikakkz@hotmail.com>
2025-12-02 12:30:01 +0800

    Remove old table

--- webui/src/controller/db.ts
+++ webui/src/controller/db.ts
@@ -7,9 +7,24 @@ export const dbKline = new Dexie('KLineDatabase') as Dexie & {
   transactions: Table<dbModel._Transaction, [string, string, number, boolean]>
 }
 
-dbKline.version(10).stores({
-  klinePoints:
+dbKline.version(11).stores({
+  klinePoints: null,
+  transactions: null,
+  klinePointsV2:
     '++id, &[token0+token1+interval+timestamp], open, close, low, high, volume, timestamp',
-  transactions:
+  transactionsV2:
     '++id, &[token0+token1+transaction_id+token_reversed], &[created_timestamp+token0+token1+token_reversed], transaction_type, from_account, amount_0_in, amount_1_in, amount_0_out, amount_1_out, liquidity, created_at, created_timestamp, price, volume, direction'
 })
+
+Object.defineProperty(dbKline, 'transactions', {
+  get() {
+    return dbKline.table('transactionsV2')
+  }
+})
+
+Object.defineProperty(dbKline, 'klinePoints', {
+  get() {
+    return dbKline.table('klinePointsV2')
+  }
+})
+
