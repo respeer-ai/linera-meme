daa16c4cb87795c13ed53f35027670341da9709a
Zhao KK <kikakkz@hotmail.com>
2025-12-04 16:11:09 +0800

    Correct ams service

--- ams/src/service.rs
+++ ams/src/service.rs
@@ -1,11 +1,14 @@
 #![cfg_attr(target_arch = "wasm32", no_main)]
 
-use abi::ams::{AmsAbi, AmsOperation};
+use abi::ams::{AmsAbi, AmsOperation, Metadata};
 use ams::state::AmsState;
-use async_graphql::{EmptySubscription, Request, Response, Schema};
+use async_graphql::{EmptySubscription, Object, Request, Response, Schema};
 use linera_sdk::{
-    graphql::GraphQLMutationRoot, linera_base_types::WithServiceAbi, views::View, Service,
-    ServiceRuntime,
+    graphql::GraphQLMutationRoot,
+    linera_base_types::WithServiceAbi,
+    linera_base_types::{ApplicationId, Timestamp},
+    views::View,
+    Service, ServiceRuntime,
 };
 use std::sync::Arc;
 
@@ -35,11 +38,85 @@ impl Service for AmsService {
 
     async fn handle_query(&self, request: Request) -> Response {
         let schema = Schema::build(
-            self.state.clone(),
+            QueryRoot {
+                state: self.state.clone(),
+            },
             AmsOperation::mutation_root(self.runtime.clone()),
             EmptySubscription,
         )
         .finish();
         schema.execute(request).await
+    }
+}
+
+struct QueryRoot {
+    state: Arc<AmsState>,
+}
+
+#[Object]
+impl QueryRoot {
+    async fn applications(
+        &self,
+        created_before: Option<Timestamp>,
+        created_after: Option<Timestamp>,
+        application_type: Option<String>,
+        spec: Option<String>,
+        application_ids: Option<Vec<ApplicationId>>,
+        limit: usize,
+    ) -> Vec<Metadata> {
+        let mut values = Vec::new();
+        self.state
+            .applications
+            .for_each_index_value_while(|_, value| {
+                if application_ids.is_some()
+                    && !application_ids
+                        .clone()
+                        .unwrap()
+                        .contains(&value.clone().application_id)
+                {
+                    return Ok(true);
+                }
+                if spec.is_some()
+                    && (!value.spec.is_some()
+                        || !value
+                            .as_ref()
+                            .clone()
+                            .spec
+                            .unwrap()
+                            .to_lowercase()
+                            .contains(&format!("\"{}\"", &spec.clone().unwrap().to_lowercase())))
+                {
+                    return Ok(true);
+                }
+                if let Some(created_before) = created_before {
+                    if value.created_at > created_before {
+                        return Ok(true);
+                    }
+                }
+                if let Some(created_after) = created_after {
+                    if value.created_at <= created_after {
+                        return Ok(true);
+                    }
+                }
+                if let Some(application_type) = application_type.clone() {
+                    if value.application_type != application_type {
+                        return Ok(true);
+                    }
+                }
+                values.push(value.as_ref().clone());
+                Ok(values.len() < limit)
+            })
+            .await
+            .expect("Failed get applications");
+        values.sort_by(|a, b| a.created_at.cmp(&b.created_at));
+        values
+    }
+
+    async fn application(&self, application_id: ApplicationId) -> Option<Metadata> {
+        self.state
+            .applications
+            .get(&application_id)
+            .await
+            .expect("Failed get application")
     }
 }
