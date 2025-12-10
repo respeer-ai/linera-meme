bee84065b67df3332347176344e8507743853a22
Zhao KK <kikakkz@hotmail.com>
2025-12-10 16:43:56 +0800

    Add header line

---
+++ webui-v2/src/components/header/ConnectWalletView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <q-btn rounded class='bg-primary flex justify-center items-center' icon='wallet'>
+      <span class='q-ml-sm q-header-line'>Connect Wallet</span>
+    </q-btn>
+  </div>
+</template>
+
+<style scoped lang='sass'>
+::v-deep(.q-btn)
+  min-height: 48px
+  line-height: 48px
+  padding: 0 16px
+  .q-icon.material-icons
+    color: white
+</style>
--- webui-v2/src/components/header/HeaderView.vue
+++ webui-v2/src/components/header/HeaderView.vue
@@ -4,10 +4,22 @@
       <div class='bg-primary-25 hover-primary radius-8 flex justify-center items-center' style='width: 48px; height: 48px;'>
         <q-img :src='laughPng' width='36px' height='36px' />
       </div>
-      <q-toolbar-title class='text-light text-h4 text-bold text-gradient-primary-secondary q-header-line'> {{ productName }} </q-toolbar-title>
+      <div class='q-ml-sm'>
+        <div class='text-light text-h5 text-bold text-gradient-primary-secondary'> {{ productName }} </div>
+        <div class='text-xs text-neutral' style='line-height: 1em;'>Creating . Trading . Mining Meme on Linera</div>
+      </div>
+    </div>
+    <div class='q-ml-lg'>
+      <tabs-view />
     </div>
     <q-space />
-    <div class='header-actions'>
+    <div>
+      <network-view />
+    </div>
+    <div class='q-ml-md'>
+      <connect-wallet-view />
+    </div>
+    <div class='header-actions q-ml-md'>
       <q-btn fab flat round icon='invert_colors' size='24px' class='bg-dark-dark' @click='onModeSwitchClick' />
     </div>
   </div>
@@ -17,6 +29,10 @@
 import { Dark } from 'quasar'
 
 import { laughPng } from 'src/assets'
+
+import TabsView from './TabsView.vue'
+import NetworkView from './NetworkView.vue'
+import ConnectWalletView from './ConnectWalletView.vue'
 
 const productName = import.meta.env.VITE_PRODUCT_NAME || import.meta.env.PRODUCT_NAME
 
