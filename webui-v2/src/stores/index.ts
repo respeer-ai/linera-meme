0f3f730bbaf0fab2a7ee8616b1786b5f759b4e5b
Zhao KK <kikakkz@hotmail.com>
2025-12-10 22:09:31 +0800

    Don't put store before pinia initialization

--- webui-v2/src/stores/index.ts
+++ webui-v2/src/stores/index.ts
@@ -30,18 +30,3 @@ export default defineStore((/* { ssrContext } */) => {
 
   return pinia;
 });
-
-export * as notify from './notify'
-export * as user from './user'
-export * as request from './request'
-export * as ams from './ams'
-export * as proxy from './proxy'
-export * as kline from './kline'
-export * as store from './store'
-export * as blob from './blob'
-export * as meme from './meme'
-export * as account from './account'
-export * as swap from './swap'
-export * as block from './block'
-export * as transaction from './transaction'
-export * as pool from './pool'
