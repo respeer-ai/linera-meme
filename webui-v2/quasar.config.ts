1a4f298c49ce62c60244bf6ffe2e4724463904dc
Zhao KK <kikakkz@hotmail.com>
2025-12-10 22:09:31 +0800

    Don't put store before pinia initialization

--- webui-v2/quasar.config.ts
+++ webui-v2/quasar.config.ts
@@ -18,7 +18,7 @@ export default defineConfig((ctx) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['i18n', 'axios'],
+    boot: ['pinia', 'i18n', 'axios'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: ['app.sass'],
