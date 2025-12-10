8dcb337569f5ad44d0ad6f5c431f6aa04431de8d
Zhao KK <kikakkz@hotmail.com>
2025-12-10 17:26:49 +0800

    Add wallet tips

--- webui-v2/src/pages/IndexPage.vue
+++ webui-v2/src/pages/IndexPage.vue
@@ -1,6 +1,13 @@
 <template>
-  <q-page class='row items-center justify-evenly' />
+  <q-page class='row justify-center'>
+    <div class='page-width q-mt-lg'>
+      <wallet-tip-view />
+    </div>
+  </q-page>
 </template>
 
 <script setup lang='ts'>
+
+import WalletTipView from 'src/components/wallet/WalletTipView.vue'
+
 </script>
