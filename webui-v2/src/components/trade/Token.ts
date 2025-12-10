ff40451ab5a49a2136d62d6235b6a3ba920b264f
Zhao KK <kikakkz@hotmail.com>
2025-12-10 19:25:31 +0800

    Add token info

---
+++ webui-v2/src/components/trade/Token.ts
@@ -0,0 +1,7 @@
+export interface Token {
+  name: string
+  description: string
+  ticker: string
+  totalSupply: number
+  image: string
+}
---
+++ webui-v2/src/components/trade/TokenInfoView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ token }}</div>
+</template>
+
+<script setup lang='ts'>
+import { toRef } from 'vue'
+import { Token } from './Token'
+
+interface Props {
+  token: Token
+}
+const props = defineProps<Props>()
+const token = toRef(props, 'token')
+
+</script>
