7203ccdc937ce83b214c8cd766f4e01761b139b7
Zhao KK <kikakkz@hotmail.com>
2025-12-10 16:43:56 +0800

    Add header line

---
+++ webui-v2/src/components/header/NetworkView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <q-select
+      filled
+      v-model='network'
+      :options='networks'
+    >
+      <template #prepend>
+        <q-img src='https://avatars.githubusercontent.com/u/107513858?s=48&v=4' width='24px' height='24px' />
+      </template>
+    </q-select>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue'
+
+enum Network {
+  TestnetConway = 'Testnet Conway',
+  Mainnet = 'Mainnet (WIP)'
+}
+
+const networks = ref([{
+  label: Network.TestnetConway,
+  value: Network.TestnetConway
+}, {
+  label: Network.Mainnet,
+  value: Network.Mainnet,
+  disable: true
+}])
+const network = ref(Network.TestnetConway)
+
+</script>
