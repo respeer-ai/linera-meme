14fa34d85042b4564fd5f973779db5ee26b465c6
Zhao KK <kikakkz@hotmail.com>
2026-01-05 16:50:57 +0800

    Ignore delete error

--- k8s/apply_webui.sh
+++ k8s/apply_webui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl delete -f webui/02-deployment.yaml
+kubectl delete -f webui/02-deployment.yaml || true
 kubectl apply -f webui/02-deployment.yaml
 kubectl apply -f webui/03-ingress.yaml
 
