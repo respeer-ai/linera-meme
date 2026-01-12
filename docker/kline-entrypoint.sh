be9a0d7280f6ff81ee9ba2a548e775ba7482128f
Zhao KK <kikakkz@hotmail.com>
2026-01-12 12:48:01 +0800

    Support retry ticker

--- docker/kline-entrypoint.sh
+++ docker/kline-entrypoint.sh
@@ -2,4 +2,8 @@
 
 python3 -u src/kline.py --swap-application-id "$SWAP_APPLICATION_ID" --database-host "$DATABASE_HOST" --database-port "$DATABASE_PORT" --database-user "$DATABASE_USER" --database-password "$DATABASE_PASSWORD" --database-name "$DATABASE_NAME" --swap-host "$SWAP_HOST" &
 sleep 10
-curl -X POST http://localhost:25080/run/ticker
+
+while true; do
+  curl -X POST http://localhost:25080/run/ticker
+  sleep 10
+done
