babcbd15f0199566390f364a2d7980c7524774ee
Zhao KK <kikakkz@hotmail.com>
2026-03-24 12:47:36 +0800

    Support two stage compilation

--- docker/build_miner.sh
+++ docker/build_miner.sh
@@ -8,16 +8,21 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$SCRIPT_DIR/..
 MINER_DIR=$ROOT_DIR/service/miner
 
-cd $MINER_DIR
+OUTPUT_DIR=$ROOT_DIR/output/compose/miner
 
-cargo build --release -j 1 --locked
+mkdir -p $OUTPUT_DIR/miner
 
-cd $ROOT_DIR
+cp $SCRIPT_DIR/Dockerfile.miner $SCRIPT_DIR/miner-entrypoint.sh $OUTPUT_DIR
+cp $MINER_DIR/build.rs $MINER_DIR/Cargo.lock $MINER_DIR/Cargo.toml $MINER_DIR/README.md $MINER_DIR/rust-toolchain.toml $MINER_DIR/src $OUTPUT_DIR/miner -rf
 
 docker rmi linera-meme-miner npool/linera-meme-miner
 
+cd $ROOT_DIR
+
 GIT_COMMIT=$(git rev-parse --short HEAD)
-docker build --build-arg git_commit="$GIT_COMMIT" -f docker/Dockerfile.miner . -t linera-meme-miner || exit 1
+
+cd $OUTPUT_DIR
+docker build --build-arg git_commit="$GIT_COMMIT" --build-arg all_proxy=$all_proxy -f Dockerfile.miner . -t linera-meme-miner || exit 1
 
 docker tag linera-meme-miner:latest docker.io/npool/linera-meme-miner:latest
 docker push npool/linera-meme-miner:latest
