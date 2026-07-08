4dfb22fbf8650c7e5d501179f1a95f4d060b1c28
Zhao KK <kikakkz@hotmail.com>
2026-07-08 18:42:09 +0800

    Re-export AMS ABI types from abi::ams module

--- abi/src/ams.rs
+++ abi/src/ams.rs
@@ -1,2 +1,12 @@
 pub mod abi;
 pub mod state_v1;
+
+pub use self::abi::{
+    AmsAbi, AmsMessage, AmsOperation, AmsResponse, APPLICATION_TYPES, InstantiationArgument,
+    Metadata, AMS, BLOB_GATEWAY, DEFI, GAME, LIQUIDITY_POOL, MEME, MEME_PROXY, OTHER, SOCIAL,
+    SWAP, UTILITY,
+};
+
+pub use self::state_v1::{
+    AmsStateAbi, AmsStateOperation, AmsStateResponse, StateInstantiationArgument,
+};
